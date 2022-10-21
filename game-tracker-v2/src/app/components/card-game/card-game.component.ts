0012732b334154d14fa27624cf39e5f1badb9a35
Camila Hatakeyama <hatakeyama.camila@gmail.com>
2022-10-21 15:40:02 -0300

    feat: cria e adiciona função que calcula o desconto

--- game-tracker-v2/src/app/components/card-game/card-game.component.ts
+++ game-tracker-v2/src/app/components/card-game/card-game.component.ts
@@ -18,6 +18,31 @@ export class CardGameComponent implements OnInit {
   @Input() oferta!: Oferta
   constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+
+  calculateDiscount(oldValue: number, newValue: number): number {
+    if (newValue < oldValue) {
+      return Math.round((oldValue - newValue) * 100 / oldValue)
+    }
+    return 0
+  }
+
+  discount(oldValue: string, newValue: string): string {
+    const oldValueNumber = this.convertTextToNumber(oldValue)
+    const newValueNumber = this.convertTextToNumber(newValue)
+    const discount = this.calculateDiscount(oldValueNumber, newValueNumber)
+    return this.displayDiscount(discount)
+  }
+
+  displayDiscount(value: number): string {
+    if (value === 100) {
+      return "GRÁTIS"
+    }
+    return `${value}%`
+  }
+
+  convertTextToNumber(textNumber: string): number {
+    return Number.parseFloat(textNumber.replace(',', '.'))
+  }
 
 }
