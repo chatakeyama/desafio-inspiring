f3f1a1a809f53ef36372ad57a188c1984e2338a3
Camila Hatakeyama <hatakeyama.camila@gmail.com>
2022-10-27 15:59:29 -0300

    feat(gameTrackerV2): troca select de ordenação nativo por customizado

--- game-tracker-v2/src/app/components/select-order/select-order.component.ts
+++ game-tracker-v2/src/app/components/select-order/select-order.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, Renderer2, ViewChild } from '@angular/core';
 import { IGameSortOption } from 'src/app/interfaces/IGameSortOption.interface';
 import { Game } from 'src/app/models/game.model';
 
@@ -10,43 +10,55 @@ import { Game } from 'src/app/models/game.model';
 export class SelectOrderComponent implements OnInit {
 
   @Output() emitSortSelection = new EventEmitter()
-
+  @ViewChild('buttonSelect') buttonSelect!: ElementRef
+  display = false
   options: IGameSortOption[] = [
     { value: 'discount', text: '% de Desconto', sortCallback: this.sortByDiscount },
     { value: 'highest-price', text: 'Maior valor', sortCallback: this.sortByHighestPrice },
     { value: 'lowest-price', text: 'Menor valor', sortCallback: this.sortByLowestPrice },
     { value: 'title', text: 'Título', sortCallback: this.sortByTitle },
   ]
+  selectedOptionText: string = this.options[0].text
 
-  constructor() { }
+  constructor(private renderer2: Renderer2) { }
 
   ngOnInit(): void {
     this.emitSortSelection.emit(this.options[0])
+    this.renderer2.listen("document", "click", event => {
+      if (!event.target.matches('.dropdown__button, .dropdown__icon')) {
+        this.display = false
+      }
+    });
   }
 
-  onSelect = (event: Event): void => {
-    const selectedValue = (event.target as HTMLInputElement).value
-    const selectedOption = this.options.find((option) => option.value === selectedValue)
-    this.emitSortSelection.emit(selectedOption)
+  onDropdownButtonClick(): void {
+    this.display = !this.display
   }
 
-  sortByLowestPrice(gamesList: Game[]) {
+  onSelectOption(value: string): void {
+    this.display = !this.display
+    const selectedOptionObj = this.options.find((option) => option.value === value)
+    this.selectedOptionText = selectedOptionObj?.text || ''
+    this.emitSortSelection.emit(selectedOptionObj)
+  }
+
+  sortByLowestPrice(gamesList: Game[]): void {
     gamesList.sort((a, b) => {
       return Number.parseFloat(a.salePrice) - Number.parseFloat(b.salePrice)
     })
   }
 
-  sortByHighestPrice(gamesList: Game[]) {
+  sortByHighestPrice(gamesList: Game[]): void {
     gamesList.sort((a, b) => {
       return Number.parseFloat(b.salePrice) - Number.parseFloat(a.salePrice)
     })
   }
 
-  sortByTitle(gamesList: Game[]) {
+  sortByTitle(gamesList: Game[]): void {
     gamesList.sort((a, b) => a.title.localeCompare(b.title))
   }
 
-  sortByDiscount(gamesList: Game[]) {
+  sortByDiscount(gamesList: Game[]): void {
     gamesList.sort((a, b) => {
       return b.getDiscount() - a.getDiscount()
     })
